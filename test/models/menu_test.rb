22df8215f932f7b59d75aa6cc3dca55c53c39031
Austin Hendricks <hendraustin@gmail.com>
2023-09-14 15:13:00 -0700

    Add Menu tests

--- test/models/menu_test.rb
+++ test/models/menu_test.rb
@@ -1,4 +1,16 @@
 require "test_helper"
 
 class MenuTest < ActiveSupport::TestCase
+  test "should save menu with name" do
+    menu = Menu.new(name: "Rails Test Menu")
+
+    assert menu.save
+    assert_equal "Rails Test Menu", menu.name
+  end
+
+  test "should not save menu without name" do
+    menu = Menu.new()
+
+    assert_not menu.save, "Saved the menu without a name"
+  end
 end
