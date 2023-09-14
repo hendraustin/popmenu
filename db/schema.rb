984e5b772110b63172f17a81163ecce4c47d3c32
Austin Hendricks <hendraustin@gmail.com>
2023-09-14 15:26:27 -0700

    Generate Restaurant model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_14_222049) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_14_222554) do
   create_table "menu_items", force: :cascade do |t|
     t.string "name"
     t.float "price"
@@ -21,6 +21,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_14_222049) do
   end
 
   create_table "menus", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
